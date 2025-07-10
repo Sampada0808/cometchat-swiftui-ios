84bc9e74ba9435f6e1d9431f22e961408923b193
Sampada0808 <sampadashankar88@gmail.com>
2025-07-10 13:52:36 +0530

    Initial Commit

---
+++ cometchat-swiftui-ios/cometchat_swiftui_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  cometchat_swiftui_iosApp.swift
+//  cometchat-swiftui-ios
+//
+//  Created by Sampada Shankar on 10/07/25.
+//
+
+import SwiftUI
+
+@main
+struct cometchat_swiftui_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
